e16840da3b4864053d56a9bf1af61f13dff309df
teom <xodn9245@naver.com>
2023-01-24 14:21:23 +0900

    2023-01-24 : 10814번

--- Baekjoon/p10814.py
+++ Baekjoon/p10814.py
@@ -1,21 +1,9 @@
 def solution(n):
     arr = [0] * n
-    min = 200
     for i in range(n):
         arr[i] = input().split(' ')
         arr[i][0] = int(arr[i][0])
-        if (arr[i][0] < min):  # 최소 나이 찾기
-            min = arr[i][0]
-    # for i in range(min, 201):
-        # if (arr.count())
-
-    # arr.sort(key=lambda arr: arr[0])
-    # print_arr(arr)
-    # print(min)
-    print(arr.count(min))
-
-
-def print_arr(arr):
+    arr.sort(key=lambda arr: arr[0])
     for i in arr:
         print("{} {}".format(i[0], i[1]))
 
