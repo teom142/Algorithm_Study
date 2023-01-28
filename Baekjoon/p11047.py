878b464992d50e0d6165a3578ee412841079721b
teom <xodn9245@naver.com>
2023-01-28 15:27:37 +0900

    2023-01-28 : 11047번 해결

--- Baekjoon/p11047.py
+++ Baekjoon/p11047.py
@@ -7,14 +7,19 @@ def solution(n, k):
     value = []
     value_cnt = [0]*n
     ans = 0
-    i = 0
+    idx = 0
     for i in range(n):
-        value.append(int(input()))
-    idx = n - 1
+        curr_val = int(input())
+        if (curr_val <= k):
+            value.append(curr_val)
+            idx += 1
+    idx -= 1
     while (idx >= 0 and k > 0):
         if (value[idx] <= k):
-            value_cnt[idx] += 1
-            k -= value[idx]
+            value_cnt[idx] = k // value[idx]
+            # // 연산으로 필요한 동전의 갯수 구함
+            k %= value[idx]
+            # % 연산으로 해당 동전의 가치만큼 k 뺄셈 연산
         else:
             idx -= 1
     for i in value_cnt:
